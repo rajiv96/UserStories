ca0206bd81553e5421a6d32ef510e25f3c18e0fc
rajiv96 <you@example.com>
2017-07-11 15:16:36 +0530

    first commit

---
+++ src/main/java/com/decoders/UserStories/UserStoriesApplication.java
@@ -0,0 +1,12 @@
+package com.decoders.UserStories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserStoriesApplication.class, args);
+	}
+}
