64c8f1ae51dc4004153053a7aeda352f88d905ad
rajiv96 <you@example.com>
2017-07-11 16:44:33 +0530

    bean soved

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users (username,password,desig) values ('user','1234','admin');
+insert into limitTrades (uid,size,Type,price,Time,currpair) values (4,'ABC','BUY',2304,'12:05:12','USD/ABC');
+insert into marketTrades (uid,size,Type,price,Time,currpair) values (4,'ABC','BUY',2304,'12:05:12','USD/ABC');
+insert into finishedTrades (buid,suid,mid,lid,currpair,size,price,Time) values (1,2,3,4,'USD','ABC',2000,'12:05:12');
+insert into historicTrades (currpair,size,price,Time) values ('sad',123,20000,'12:05:12');
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,59 @@
+drop table users if exists;
+drop table limitTrades if exists;
+drop table marketTrades if exists;
+drop table finishedTrades if exists;
+drop table historicTrades if exists;
+
+create table users(
+uid int(255) NOT NULL AUTO_INCREMENT,
+username varchar(255) NOT NULL,
+password varchar(255) NOT NULL,
+desig varchar(255) NOT NULL,
+PRIMARY KEY (uid)
+);
+
+
+create table limitTrades(
+lid int(255) NOT NULL AUTO_INCREMENT,
+uid int(255) NOT NULL,
+size varchar(255) NOT NULL,
+Type varchar(255) NOT NULL,
+price double(255) NOT NULL,
+Time time,
+currpair varchar(255),
+PRIMARY KEY (lid)
+);
+
+
+create table marketTrades(
+mid int(255) NOT NULL AUTO_INCREMENT,
+uid int(255) NOT NULL,
+size varchar(255) NOT NULL,
+Type varchar(255) NOT NULL,
+price double(255) NOT NULL,
+Time time,
+currpair varchar(255),
+PRIMARY KEY (mid)
+);
+
+create table finishedTrades(
+fid int(255) NOT NULL AUTO_INCREMENT,
+buid int(255) NOT NULL,
+suid int(255) NOT NULL,
+mid int(255) NOT NULL,
+lid int(255) NOT NULL,
+currpair varchar(255),
+size varchar(255) NOT NULL,
+price double(255) NOT NULL,
+Time time,
+PRIMARY KEY (fid)
+);
+
+create table historicTrades(
+hid int(255) NOT NULL AUTO_INCREMENT,
+currpair varchar(255),
+size int(255) NOT NULL,
+price double(255) NOT NULL,
+Time time,
+PRIMARY KEY (hid)
+);
